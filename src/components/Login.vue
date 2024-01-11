f56feffcb2664ccc8e229ba31806a3b1ab92af9b
Tor Rydberg <crownroyalsuperslut@yahoo.com>
2024-01-11 10:52:32 -0500

    added login form, not functionality

---
+++ src/components/Login.vue
@@ -0,0 +1,45 @@
+<script setup>
+import {   ref } from 'vue'
+import  * as APICalls from '../api.js'
+
+let userName = ref("");
+let pword = ref("");
+
+
+
+function checkCreds(userName, pword) {
+  console.log(userName)
+  console.log(pword)
+  // fetch('src/api.js'), {
+  //   method: 'POST',
+  //   headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //   body: JSON.stringify({
+  //    'userName': userName,
+  //    'pasword': pword
+  //   })
+  //}
+ 
+  
+    }
+
+
+</script>
+
+<template>
+<form >
+  <label for="userName">UserName:</label>
+  <input v-model="userName" type="text" id="userName">
+  <input v-model="pword" type="password" id="pword">
+  <input type="submit" value="Submit"
+  @click.prevent="checkCreds(userName, pword)"
+  >
+</form>
+  
+ 
+</template>
+
+<style scoped>
+
+</style>
