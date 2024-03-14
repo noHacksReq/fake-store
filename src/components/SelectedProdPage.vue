460a9f45f4efefddd25e8d8ea750b2314c0ed2b2
Tor Rydberg <crownroyalsuperslut@yahoo.com>
2024-03-14 11:10:02 -0400

    added functionality to toggle single product page

---
+++ src/components/SelectedProdPage.vue
@@ -0,0 +1,34 @@
+<script setup>
+import {   ref } from 'vue'
+import  * as APICalls from '../api.js'
+
+import { useProdsStore } from '../productStore.js'
+const store = useProdsStore()
+
+
+
+
+</script>
+
+<template>
+  <section v-if="store.selectedItem !== null" class="selectedProdPage">
+    <h1>{{ store.selectedItem.title }}</h1>
+      <img class="modalImage" :src="`${store.selectedItem.image}`">
+      <p>{{ store.selectedItem.description }}</p>
+      <h3>Price: ${{ store.selectedItem.price }}</h3>
+    <p @click="store.toggleSelectedPage()">
+      back to {{ store.selectedCat }}
+    </p>
+  </section>
+  
+
+</template>
+
+<style scoped>
+.prodPage {
+
+}
+
+
+
+</style>
